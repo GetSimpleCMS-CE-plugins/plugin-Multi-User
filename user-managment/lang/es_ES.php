728f6ab4195fd9fbbc7c99dda34a8e2e8499ed35
risingisland <risingisland@yahoo.com>
2023-07-22 10:55:27 +0100

    Add files via upload

---
+++ user-managment/lang/es_ES.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Multi User Spanish language file
+ */
+
+$i18n = [
+	'TITLE' => "Gestor de Usuarios",
+	'SIDEBAR' => "Gestor de <em>U</em>suarios",
+	'PLUGIN_DESCRIPTION' => "Agrega Multi-User Management - Editar todas las opciones para los usuarios actuales y gestionar los permisos.",
+	'ADDUSER' => "Añadir Nuevo Usuario",
+	'EDITUSER' => "Editar usuario",
+	'UPDATED' => "El plugin se ha actualizado con éxito.",
+	'UPDATEERROR' => "ERROR! - Script no pudo extraer y CHMOD el archivo.",
+	'EDIT' => "Editar",
+	'SHOW' => "mostrar",
+	'HIDE' => "ocultar",
+	'PERM' => "Permisos denegados",
+	'LAND' => "Página de inicio personalizada",
+	'ADMIN' => "Administrar usuarios",
+	'SAVE' => "Guardar cambios",
+	'DELETE' => "Borrar usuario",
+	'DELETED_P' => "Usuario",
+	'DELETED' => "ha sido eliminado con éxito.",
+	'DELETEERROR' => "ERROR! - No se puede eliminar el archivo, compruebe el registro de errores o active el modo de depuración.",
+	'SAVEERROR' => "ERROR! - No guardo el archivo",
+	'SAVED' => "Se han guardado los cambios.",
+	'CREATED_P' => "Usuario",
+	'CREATED' => "ha sido creado.",
+	'DELETESURE' => "Estas seguro que quieres borrarlo",
+	'USER_BIO'   => "Informacion personal",
+	'USERBIO_LABEL'   => "Se puede utilizar como un comentario personal del usuario o como un perfil del usuario para su uso en páginas públicas, como en el módulo GetSimple Blog. El enlace a la derecha activa el editor de HTML.",
+	'NAME' => "Nombre",
+	'HTML_ED' => "Editor de HTML",
+	'ADMIN_FLAG' => "admin",
+	'LOGEDIN' => "Usuario actual",
+	'CANNOT_DENY' => "No puede negar estos derechos para el usuario que ha iniciado sesión.",
+	'WEBSETTINGS_LABEL' => "Oculta solo la configuración del sitio web.
+	La configuración actual del usuario permanece disponible.",
+	'LANDINGPAGE_LABEL' => "Si niega el acceso a Pages, elija otra página de destino para el usuario según su función.",
+	'EDIT_PAGE' => "Editar o crear página",
+	'CUSTOM_PERM' => "Denegar permisos para módulos:",
+	'ERROR_XML' => "¡No se puede cargar el archivo XML!",
+	'ERROR_LNG' => "No se puede cargar el archivo de idioma ",
+	'ERROR_USR' => "No puede crear un usuario sin nombre de usuario o contraseña.",
+	'NO_PERMISSION' => "No tienes permisos para estas configuraciones."
+];
